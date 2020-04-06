ab4371fe578f23cc889b05ca9948a72a975b436e
PlaybackPR <playback3000@gmail.com>
2020-04-06 16:02:43 +0300

    chat ver. 005

--- chat/client.py
+++ chat/client.py
@@ -1,11 +1,7 @@
 import socket
-
-HOST = '172.17.9.193'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-
-print('Received', repr(data))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('172.17.9.193', 8111))
+client.send("I am CLIENT<br>")
+from_server = client.recv(4096)
+client.close()
+print (from_server)
--- chat/server.py
+++ chat/server.py
@@ -1,16 +1,15 @@
 import socket
-
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv.bind(('172.17.9.193', 8111))
+serv.listen(5)
+while True:
+    conn, addr = serv.accept()
+    from_client = ''
+    while True:
+        data = conn.recv(4096)
+        if not data: break
+        from_client += data
+        print (from_client)
+        conn.send("I am SERVER<br>")
+    conn.close()
+    print ('client disconnected')
