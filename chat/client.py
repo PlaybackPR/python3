0b199f0c7f9b6570d5de09aff86e0b142575ad90
PlaybackPR <playback3000@gmail.com>
2020-04-06 15:37:53 +0300

    chat ver. 002

--- chat/client.py
+++ chat/client.py
@@ -4,11 +4,11 @@ def read_sok():
      while 1 :
          data = sor.recv(1024)
          print(data.decode('utf-8'))
-         server = '192.168.0.109', 5050  # Данные сервера
-         alias = input() # Вводим наш псевдоним
+         server = '172.17.9.193', 5050
+         alias = input()
          sor = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-         sor.bind(('', 0)) # Задаем сокет как клиент
-         sor.sendto((alias+' Connect to server').encode('utf-8'), server)# Уведомляем сервер о подключении
+         sor.bind(('', 0))
+         sor.sendto((alias+' Connect to server').encode('utf-8'), server)
          potok = threading.Thread(target= read_sok)
          potok.start()
      while 1 :
